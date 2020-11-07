f50d7d4aae9b26889526a9c038e798a4f82c0f02
prabodh44 <prabodhtuladhar44@gmail.com>
2020-11-07 17:22:13 +0545

    added detail views, fieldsets in the admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,7 +5,8 @@ from catalog.models import Author, Genre, Book, BookInstance
 
 # admin.site.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    list_display = ('last_name', 'first_name','date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 admin.site.register(Author, AuthorAdmin)
 # registering new models using the @register deocorator
@@ -18,7 +19,14 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     # adding list filters using list_filter
-    list_filter = ('status', 'due_date')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields' : ('book', ('id', 'imprint'))}), 
+        ('Availability', {
+             'fields' : ('status', 'due_back')   
+            })
+    )
 
 admin.site.register(Genre)
 # admin.site.register(Author)

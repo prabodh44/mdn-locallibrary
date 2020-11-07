dc20b6d67c52e35f7fb385842c06a2db310d0173
prabodh44 <prabodhtuladhar44@gmail.com>
2020-11-07 16:48:47 +0545

    added modeladmin | added models to admin suing decorators and ModelAdmin Class

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,8 +3,25 @@ from django.contrib import admin
 from catalog.models import Author, Genre, Book, BookInstance
 # Register your models here.
 
-admin.site.register(Author)
+# admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+
+admin.site.register(Author, AuthorAdmin)
+# registering new models using the @register deocorator
+# does the same thing as above
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Genre)
-admin.site.register(Book)
-admin.site.register(BookInstance)
+# admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
 

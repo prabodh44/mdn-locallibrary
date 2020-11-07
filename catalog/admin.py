371b2f0389452243b246684d157f5e2b6ccfe21a
prabodh44 <prabodhtuladhar44@gmail.com>
2020-11-07 10:14:46 +0545

    Added the models to the admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from catalog.models import Author, Genre, Book, BookInstance
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(BookInstance)
+

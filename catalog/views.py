18744c4df54153aac7180017c54cfffdf4ac720c
prabodh44 <prabodhtuladhar44@gmail.com>
2020-11-07 19:32:50 +0545

    Added the index view

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from catalog.models import BookInstance, Book, Author, Genre
 # Create your views here.
+
+def indext(request):
+    num_of_books        = Book.objects.all().count()
+    num_of_instances    = BookInstance.objects.all().count()
+    
+    # Available books (status = a)  
+    num_of_instances_avaiable = BookInstance.objects.filter(stauts__exact='a').count()
+    
+    num_of_authors = Author.objects.all().count()
+    
+    context = {
+        'num_of_books' : num_of_books,
+        'num_of_instances' : num_of_instances,
+        'num_of_instances_avaiable' : num_of_instances_avaiable,
+        'num_of_authors': num_of_authors,
+    }
+    
+    return render(request, 'index.html', context)
+    
